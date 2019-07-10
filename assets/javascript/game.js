7a2c20428e1f49704be737fa93104b84222cbb1b
Zuzanav <zuzanavolny@gmail.com>
2019-07-09 22:16:21 -0700

    working wins, losses and guesses. Guessed letters tbd

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,50 @@
-// Wins: (# of times the user has guessed the letter correctly)
 
-// Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-
-// Guesses Left: (# of guesses left. This will update)
-
-// Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
-// When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
-// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
-
-// ====================================
-
+// Load when DOM is ready
 $(document).ready(function() {
 
 // Array with all letters of the alphabet for computer to choose from 
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Variables for scores
+// Variables for Scores
     var wins = 0;
     var losses = 0;
     var guesses = 9;
 
+// Variables for HTML Text
     var winsText = document.getElementById("wins");
     var lossesText = document.getElementById("losses");
     var guessesText = document.getElementById("guesses");
 
+// Starting the Game with 0 wins, 0 losses and 9 Guesses
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesText.textContent = "Guesses Left: " + guesses;
 
-    // watch for user to press key
+// Computer chooses a letter, randomly
+    var compGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log ("computer guess: " + compGuess);
+
+// Watch for user to press a key
     document.onkeyup = function(event) {
 
-        // save pressed key
+    // save pressed key
         var userGuess = event.key;
-        console.log (userGuess);
-  
-        // Computer chooses a letter, randomly
-        var compGuess = letters[Math.floor(Math.random() * letters.length)];
-        console.log (compGuess);
+        console.log ("user guess: " + userGuess);
   
         // Compare the guesses
         if (userGuess === compGuess)  {
           // Add to Wins
           wins++;
           winsText.textContent = "Wins: " + wins;
-          console.log (wins);
+          compGuess = letters[Math.floor(Math.random() * letters.length)];
+          console.log ("computer guess win: " + compGuess);
+          console.log ("Wins: " + wins);
 
           // Otherwise, amount of guesses goes down
         } else {
             guesses--;
-            console.log (guesses);
+            guessesText.textContent = "Guesses Left: " + guesses;
+            console.log ("Guesses left: " + guesses);
             }
 
         // when user is out of guesses, they lose and the game resets
@@ -56,13 +52,13 @@ $(document).ready(function() {
             losses++;
             guesses = 9
             lossesText.textContent = "Losses: " + losses;
+
+            compGuess = letters[Math.floor(Math.random() * letters.length)];
+            console.log ("computer guess lose: " + compGuess);
         }
+
         
         }
-
-        // Display Score Board
-        // winsText.textContent = "Wins: " + wins;
-
 
 
     });
